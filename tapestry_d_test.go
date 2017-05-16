10e448405ff771b41e720ae7d8369d49a8bd0a16
Aaron Elkins <threcius@yahoo.com>
2017-05-16 18:47:06 +0800

    Add test case for getInterfaces()

--- tapestry_d_test.go
+++ tapestry_d_test.go
@@ -13,6 +13,13 @@ func equal(a []int32, b []int32) bool {
 	return true
 }
 
+func validInterfaceName(n string) bool {
+	if n == "lo0" || n == "en0" || n == "en1" {
+		return true
+	}
+	return false
+}
+
 func TestReversePids(t *testing.T) {
 	example := []int32{1, 2, 3, 4, 5}
 	result := []int32{5, 4, 3, 2, 1}
@@ -20,5 +27,16 @@ func TestReversePids(t *testing.T) {
 
 	if !equal(result, example) {
 		t.Errorf("reversePids not working!\n")
+	}
+}
+
+func TestGetInterfaces(t *testing.T) {
+	interfaces := getInterfaces()
+
+	for _, intf := range interfaces {
+		name := intf.Name
+		if !validInterfaceName(name) {
+			t.Errorf("getInterfaces() testing failed\n")
+		}
 	}
 }
